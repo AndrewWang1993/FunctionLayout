72aa7765535f96fdf1e9548f3966f9ee9da3dab1
wangxiaoming <wangxiaoming@qihan.com>
2016-04-20 20:05:25 +0800

    remove log

--- Demo/src/main/java/xiaoming/website/demo/MainActivity.java
+++ Demo/src/main/java/xiaoming/website/demo/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
@@ -37,7 +36,6 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     protected void onResume() {
         HashMap<Integer, ArrayList<Object>> map = restoreRecentFunction();
         ArrayList<Integer> sortIndex = restoreFunctionSort();
-        Log.i("peoce", "restore   --->" + map.toString());
         if (map != null && sortIndex != null) {
             mRecentLayout.setData(map, sortIndex);
         } else {
